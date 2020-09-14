33be61bd3a4514b5f23d9a8c07f6a767f6aa97a7
dtonthat2 <70494017+dtonthat2@users.noreply.github.com>
2020-09-13 19:31:56 -0700

    Add files via upload

---
+++ chem160homework2/dice.py
@@ -0,0 +1,12 @@
+from random import choices
+nrolls = 15000
+player1wins = 0
+for i in range(nrolls):
+    dice2 = choices(range(1,7),k = 2)
+    if dice2[0] == dice2[1]:
+        continue
+    dice1 = choices(range(1,7),k = 3)
+    dice1.sort(reverse=True)
+    if dice1[0]+dice1[1] > dice2[0]+dice2[1]:
+        player1wins =  player1wins+1
+print("Ratio of player1 wins =",player1wins/nrolls)
